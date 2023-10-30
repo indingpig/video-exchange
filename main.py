f5879f31e32e1abf751b7b66685f3d2d937734fc
魔法师 <indoing@gmail.com>
2023-10-30 11:19:11 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-print('hello word')
+import os
 
-print('hello word')
 
-print('hello word')
+# print(os.getcwd())
+# # print(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+# path = os.path.abspath(os.path.dirname((os.path.dirname(os.getcwd()))))
+# print(path)
+
+# files = os.listdir(path + '/tsTest/beginner')
+# print(files)
+
+# for file in files:
+#   if not os.path.isdir(file):
+#     f = open(path + '/tsTest/beginner/' +  file)
+#     lines = f.read()
+#     print(lines)
+#     f.close()
+
+os.system('whisper -h')
